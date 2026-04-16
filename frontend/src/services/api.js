a84e493e6b3156230154fd5d24160f36d8419e48
Jeffrin Samuel <jeffrinsamuel2006@gmail.com>
2026-04-16 22:38:44 +0530

    hardcode backend url for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,8 +4,7 @@
 
 import axios from "axios";
 
-const BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const BASE_URL = "https://expense-tracker-app-avdu.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
